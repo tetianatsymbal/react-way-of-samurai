7f00c4e5760c13bfd74b8e5c4674a8bb785ae299
Tetiana Tsymbal <77539726+tetianatsymbal@users.noreply.github.com>
2022-09-04 19:56:47 +0300

    implementation of Preloader, React.lazy and Suspense

--- src/App.js
+++ src/App.js
@@ -1,76 +1,89 @@
-import React, { Component } from "react";
+import React, { Component, Suspense } from "react";
 import { connect, Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import store from "./redux/redux-store";
 import { compose } from "redux";
 import "./App.css";
 import Preloader from "./components/common/preloader/Preloader";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/Login";
 import Music from "./components/Music/Music";
 import Navbar from "./components/Navbar/Navbar";
 import News from "./components/News/News";
-import ProfileContainer, { withRouter } from "./components/Profile/ProfileContainer";
 import Settings from "./components/Settings/Settings";
+// import DialogsContainer from "./components/Dialogs/DialogsContainer";
+// import ProfileContainer from "./components/Profile/ProfileContainer";
+// import UsersContainer from "./components/Users/UsersContainer";
+import { withRouter } from "./components/Profile/ProfileContainer";
 import SidebarContainer from "./components/Sidebar/SidebarContainer";
-import UsersContainer from "./components/Users/UsersContainer";
-import { initializeAppThunk } from './redux/app-reducer';
+import { initializeAppThunk } from "./redux/app-reducer";
+const ProfileContainer = React.lazy(() =>
+  import("./components/Profile/ProfileContainer")
+);
+const UsersContainer = React.lazy(() =>
+  import("./components/Users/UsersContainer")
+);
+const DialogsContainer = React.lazy(() =>
+  import("./components/Dialogs/DialogsContainer")
+);
 
 class App extends Component {
-  
   componentDidMount() {
     this.props.initializeAppThunk();
   }
 
   render() {
-    if(!this.props.initialized){
-      return <Preloader/>
+    if (!this.props.initialized) {
+      return <Preloader />;
     }
     return (
       <div className="app-wrapper">
         <HeaderContainer />
         <Navbar />
-        <SidebarContainer/>
+        <SidebarContainer />
         <div className="app-wrapper-content">
-          <Routes>
-            <Route
-              path="/dialogs/*"
-              element={<DialogsContainer />} />
-            <Route
-              path="/profile"
-              element={<ProfileContainer key={1} />} exact/>
-            <Route
-              path="/profile/:userId"
-              element={<ProfileContainer key={2} />} />
-            <Route path="/users/*" element={<UsersContainer />} />
-            <Route path="/news/*" element={<News />} />
-            <Route path="/music/*" element={<Music />} />
-            <Route path="/login/*" element={<LoginPage />} />ы
-            <Route path="/settings/*" element={<Settings />} />
-
-          </Routes>
+          <Suspense fallback={<Preloader/>}>
+            <Routes>
+              <Route path="/dialogs/*" element={<DialogsContainer />} />
+              <Route
+                path="/profile"
+                element={<ProfileContainer key={1} />}
+                exact
+              />
+              <Route
+                path="/profile/:userId"
+                element={<ProfileContainer key={2} />}
+              />
+              <Route path="/users/*" element={<UsersContainer />} />
+              <Route path="/news/*" element={<News />} />
+              <Route path="/music/*" element={<Music />} />
+              <Route path="/login/*" element={<LoginPage />} />ы
+              <Route path="/settings/*" element={<Settings />} />
+            </Routes>
+          </Suspense>
         </div>
       </div>
     );
-  };
-};
+  }
+}
 
 const mapStateToProps = (state) => ({
   initialized: state.app.initialized,
-})
+});
 
-const AppContainer =  compose(
+const AppContainer = compose(
   withRouter,
-  connect(mapStateToProps, {initializeAppThunk}))
-  (App);
+  connect(mapStateToProps, { initializeAppThunk })
+)(App);
 
 const MainApp = (props) => {
-  return <BrowserRouter>
-  <Provider store={store}>
-    <AppContainer/>
-  </Provider>
-</BrowserRouter>
-}
+  return (
+    <BrowserRouter>
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
+    </BrowserRouter>
+  );
+};
 
-export default  MainApp;
+export default MainApp;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -16,7 +16,7 @@ let Users = ({
       <Paginator
         currentPage={currentPage}
         onPageChanged={onPageChanged}
-        totalUsersCount={totalUsersCount}
+        totalItemsCount={totalUsersCount}
         pageSize={pageSize}
       />
       <div className={s.users}>
